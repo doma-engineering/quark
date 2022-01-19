7659ee03ead914290feadaaef44152ff79ca2d7b
conflagrate <jm@memorici.de>
2022-01-19 21:41:58 +0000

    Problem: Kernel broke Witchcraft at 1.12+
    Solution:
    
     - We need to release a fixed version of witchcraft
     - It's fixed in upstream
     - It depends on quark
     - Other stuff also depends on quark
     - Thus, to start experimenting with bumping witchcraft, it makes sense to bump quark first. (Also, it's outright necessary in this case).

--- mix.exs
+++ mix.exs
@@ -3,39 +3,36 @@ defmodule Quark.Mixfile do
 
   def project do
     [
-      app:  :quark,
+      app: :quark,
       name: "Quark",
-
-      description: "Common combinators for Elixir",
-      version: "2.3.1",
-      elixir:  "~> 1.11",
-
+      description:
+        "Common combinators for Elixir, forked by doma for maintenance and testing packagesets by doma.",
+      version: "2.3.2-doma",
+      elixir: "~> 1.11",
       package: [
-        maintainers: ["Brooklyn Zelenka"],
-        licenses:    ["MIT"],
-        links:       %{"GitHub" => "https://github.com/expede/quark"}
+        name: "doma_quark",
+        maintainers: ["doma.dev"],
+        licenses: ["MIT"],
+        links: %{
+          "Upstream" => "https://github.com/witchcrafters/quark",
+          "GitHub" => "https://github.com/doma-engineering/quark"
+        }
       ],
-
-      source_url:   "https://github.com/expede/quark",
-      homepage_url: "https://github.com/expede/quark",
-
+      source_url: "https://github.com/witchcrafters/quark",
+      homepage_url: "https://github.com/witchcrafters/quark",
       aliases: [
         quality: [
           "test",
           "credo --strict"
         ]
       ],
-
       deps: [
-        {:credo,    "~> 1.5",  only: [:dev, :test], runtime: false},
-
-        {:dialyxir, "~> 1.1",  only: :dev, runtime: false},
-        {:earmark,  "~> 1.4",  only: :dev, runtime: false},
-        {:ex_doc,   "~> 0.23", only: :dev, runtime: false},
-
-        {:inch_ex,  "~> 2.0",  only: [:dev, :docs, :test], runtime: false}
+        {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+        {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+        {:earmark, "~> 1.4", only: :dev, runtime: false},
+        {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+        {:inch_ex, "~> 2.0", only: [:dev, :docs, :test], runtime: false}
       ],
-
       docs: [
         extras: ["README.md"],
         logo: "./brand/logo.png",
